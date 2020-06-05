2e77d330c963fce3db2161aab156e82986bab0a5
Lastorder <18280396+Lastorder-DC@users.noreply.github.com>
2020-06-05 16:19:19 +0900

    Fix changed value
    Changed from upstream PR 11853

--- src/openrct2/network/Twitch.cpp
+++ src/openrct2/network/Twitch.cpp
@@ -197,7 +197,7 @@ namespace Twitch
     }
 
     /**
-     * GET /leave/:join
+     * GET /join/:join
      */
     static void Join()
     {
@@ -245,7 +245,7 @@ namespace Twitch
     }
 
     /**
-     * GET /leave/:channel
+     * Simply disable twitch integration
      */
     static void Leave()
     {
@@ -254,22 +254,6 @@ namespace Twitch
         _twitchState = TWITCH_STATE_LEFT;
         _twitchLastPulseTick = 0;
         gTwitchEnable = false;
-
-        // TODO reset all peeps with twitch flag
-
-        // HTTP request no longer used as it could be abused
-        // char url[256];
-        // snprintf(url, sizeof(url), "%sleave/%s", TwitchExtendedBaseUrl, gConfigTwitch.channel);
-        // _twitchState = TWITCH_STATE_LEAVING;
-        // _twitchIdle = false;
-        // http_request_json_async(url, [](http_json_response * jsonResponse) -> void
-        // {
-        //     http_request_json_dispose(jsonResponse);
-        //     _twitchState = TWITCH_STATE_LEFT;
-        //     _twitchIdle = true;
-        //
-        //     GetContext()->WriteLine("Left twitch channel.");
-        // });
     }
 
     static void Get(const std::string& requestFormat, int operation)
@@ -423,12 +407,12 @@ namespace Twitch
                     }
                 }
 
-                if (peep->peep_flags & PEEP_FLAGS_TWITCH)
+                if (peep->PeepFlags & PEEP_FLAGS_TWITCH)
                 {
                     if (member == nullptr)
                     {
                         // Member no longer peep name worthy
-                        uint32_t flags = peep->peep_flags & ~(PEEP_FLAGS_TRACKING | PEEP_FLAGS_TWITCH);
+                        uint32_t flags = peep->PeepFlags & ~(PEEP_FLAGS_TRACKING | PEEP_FLAGS_TWITCH);
 
                         auto guestSetFlagsAction = GuestSetFlagsAction(peep->sprite_index, flags);
                         GameActions::Execute(&guestSetFlagsAction);
@@ -437,7 +421,7 @@ namespace Twitch
                     }
                     else
                     {
-                        uint32_t flags = peep->peep_flags;
+                        uint32_t flags = peep->PeepFlags;
                         if (member->ShouldTrack)
                         {
                             flags |= (PEEP_FLAGS_TRACKING);
@@ -446,17 +430,17 @@ namespace Twitch
                         {
                             flags &= ~(PEEP_FLAGS_TRACKING);
                         }
-                        if (flags != peep->peep_flags)
+                        if (flags != peep->PeepFlags)
                         {
                             auto guestSetFlagsAction = GuestSetFlagsAction(peep->sprite_index, flags);
                             GameActions::Execute(&guestSetFlagsAction);
                         }
                     }
                 }
-                else if (member != nullptr && !(peep->peep_flags & PEEP_FLAGS_LEAVING_PARK))
+                else if (member != nullptr && !(peep->PeepFlags & PEEP_FLAGS_LEAVING_PARK))
                 {
                     // Peep with same name already exists but not twitch
-                    uint32_t flags = peep->peep_flags | PEEP_FLAGS_TWITCH;
+                    uint32_t flags = peep->PeepFlags | PEEP_FLAGS_TWITCH;
                     if (member->ShouldTrack)
                     {
                         flags |= PEEP_FLAGS_TRACKING;
@@ -488,7 +472,7 @@ namespace Twitch
                 }
 
                 AudienceMember* member = &members[memberIndex];
-                if (peep->name == nullptr && !(peep->peep_flags & PEEP_FLAGS_LEAVING_PARK))
+                if (peep->name == nullptr && !(peep->PeepFlags & PEEP_FLAGS_LEAVING_PARK))
                 {
                     // Rename peep and add flags
                     auto memLen = std::strlen(member->Name) + 1;
@@ -497,7 +481,7 @@ namespace Twitch
                     {
                         std::memcpy(peep->name, member->Name, memLen);
 
-                        uint32_t flags = peep->peep_flags | PEEP_FLAGS_TWITCH;
+                        uint32_t flags = peep->PeepFlags | PEEP_FLAGS_TWITCH;
                         if (member->ShouldTrack)
                         {
                             flags |= PEEP_FLAGS_TRACKING;
@@ -560,8 +544,6 @@ namespace Twitch
             safe_strcpy(buffer + 1, message, sizeof(buffer) - 1);
 
             utf8_remove_formatting(buffer, false);
-
-            // TODO Create a new news item type for twitch which has twitch icon
             news_item_add_to_queue_raw(NEWS_ITEM_BLANK, buffer, 0);
         }
     }
